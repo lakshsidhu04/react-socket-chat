0af43b15b5806bfe0f07d02ebe126e422cca2c97
lakshsidhu04 <cs23btech11031@iith.ac.in>
2024-07-08 10:26:36 +0530

    fixed the sidebar bug

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -1,16 +1,24 @@
-// components/Container.jsx
-
 import React, { useState } from 'react';
 import SideBar from './SideBar';
-import Chat from './Chat';
+import ChatWindow from './ChatWindow';
 
 const Container = () => {
   const [targetUser, setTargetUser] = useState(null);
 
   return (
     <div className="flex h-screen">
-      <SideBar setTargetUser={setTargetUser} />
-      {targetUser && <Chat targetUser={targetUser} />}
+      <div className="w-1/4">
+        <SideBar setTargetUser={setTargetUser} />
+      </div>
+      <div className="w-3/4">
+        {targetUser ? (
+          <ChatWindow targetUser={targetUser} />
+        ) : (
+          <div className="flex items-center justify-center h-full">
+            <p>Select a user to start chatting</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
